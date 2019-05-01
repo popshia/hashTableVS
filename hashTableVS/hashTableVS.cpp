483a1242f9ec5b467f3a20a9fef67277675164ac
popshia <B123292179@gmail.com>
2019-05-01 15:48:10 +0800

    function 1 small progress

--- hashTableVS/hashTableVS.cpp
+++ hashTableVS/hashTableVS.cpp
@@ -1,4 +1,5 @@
-// 10627130 ªL«a¨} & 10627131 §õ®mÞ³ // CodeBlocks 17.12
+// 10627130 ªL«a¨} & 10627131 §õ®mÞ³ // Visual Studio Community 2019
+#define _CRT_SECURE_NO_DEPRECATE
 #include <iostream>
 #include <sstream>
 #include <fstream>
@@ -26,21 +27,28 @@ typedef struct DataStruct {
 	float average;
 } DataStruct;
 
-static ifstream input;
-static ofstream output;
 static string FileNumber = "0";
 static int Count = 1;
 static int Command = 0;
-
-class function0 {
-
-};
+static FILE* openBin = false;
+static FILE* openTxt = false;
 
 class function1 {
+	DataStruct 
+	void ReadBin() {
+		;
+	} // ReadBin()
+
+	void ReadTxt() {
+		fscanf()
+	}
+};
+
+class function2 : public function1 {
 
 };
 
-class function2 {
+class function3 : public function2 {
 
 };
 
@@ -48,9 +56,9 @@ int main() {
 	bool continueOrNot = false;
 
 	do {
-		function0 Zero;
 		function1 One;
 		function2 Two;
+		function3 Three;
 		cout << "**********************************************" << endl; // welcome message
 		cout << "*****               DS2ex03              *****" << endl;
 		cout << "***** 0 : Quit                           *****" << endl;
@@ -63,80 +71,89 @@ int main() {
 		cin >> Command; // read in user command
 		cout << endl;
 
-		if (Command == 0) { // bye :(((
+		if ( Command == 0 ) { // bye :(((
 			cout << "Bye :(((" << endl;
 			return 0;
 		} // quit
 
-		else if (Command > 3 || Command < 0) {
+		else if ( Command > 3 || Command < 0 ) {
 			cout << "Error command! please enter an acceptable command:" << endl << endl;
 			continueOrNot = true;
 		} // wrong command
 
-		else if (Command == 1) { // read, count and build
+		else if ( Command == 1 ) { // read, count and build
 			bool function1Confirm = false;
-
+			
 			do {
-				cout << "Please enter the file you want to build a 2-3 Tree or [0] to quit:" << endl;
+				cout << "Please enter the file number you want to tranform to binary file or [0] to quit:" << endl;
 				cin >> FileNumber;
 
-				if (FileNumber == "0") {
+				if ( FileNumber == "0" ) {
 					function1Confirm = true;
 					continueOrNot = true;
+					cout << endl;
 				} // quit
 
 				else {
 					string txtFileName = "input" + FileNumber + ".txt";
 					string binFileName = "input" + FileNumber + ".bin";
-					input.open(txtFileName.c_str());
-					if (input.is_open()) {
-						// One.ReadNAnalyze() ;
+					openBin = fopen( binFileName.c_str(), "rb" );
+
+					if ( openBin ) {
+						//One.ReadBin();
 						function1Confirm = true;
 						continueOrNot = true;
+					} // binary file open successfully
+
+					else {
+						openTxt = fopen( txtFileName.c_str(), "r" );
+						if ( openTxt ) {
+							//One.ReadTxt();
+							function1Confirm = true;
+							continueOrNot = true;
+						} // txt file open sucessfully
+						else {
+							cout << "*****  file does not exist!  *****" << endl;
+							function1Confirm = false;
+						} // no such file
 					} // open successfully
-					input.open(binFileName.c_str());
-					if (input.is_open()) {
-						// One.ReadNAnalyze() ;
-						function1Confirm = true;
-						continueOrNot = true;
-					} // open successfully
-					else cout << "*****  file does not exist!  *****" << endl;
 				} // continue
-			} while (!function1Confirm);
+			} while ( !function1Confirm );
 
 			Count = 1;
 			FileNumber = "0";
-			input.close();
-			output.close();
-		} // 2-3 tree
+			if ( openBin ) fclose( openBin );
+			if ( openTxt ) fclose( openTxt );
+		} // function 1
 
-		else if (Command == 2) {
+		else if ( Command == 2 ) {
 			bool function2Confirm = false;
 
 			do {
 				cout << "Please enter the file you want to build a AVL Tree or [0] to quit:" << endl;
 				cin >> FileNumber;
 
-				if (FileNumber == "0") {
+				if ( FileNumber == "0" ) {
 					function2Confirm = true;
 					continueOrNot = true;
 				} // quit
 
 				else {
 					string fileName = "input" + FileNumber + ".txt";
-					input.open(fileName.c_str());
-					if (input.is_open()) {
+					if (1) {
 						function2Confirm = true;
 						continueOrNot = true;
 					} // open successfully
 					else cout << "*****  " << fileName << " does not exist!  *****" << endl;
 				} // open file and input data to BST
-			} while (!function2Confirm);
+			} while ( !function2Confirm );
 
 			Count = 0;
 			FileNumber = "0";
-			input.close();
-			output.close();
 		} // AVL Tree
-	} while (continueOrNot);
+
+		else if ( Command == 3 ) {
+			;
+		}
+	} while ( continueOrNot );
 }
